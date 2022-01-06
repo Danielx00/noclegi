1548948badd8d6c90f3c46c03012ad87fa1837d5
Daniel Młynarczyk <d.mlynarczyk16@gmail.com>
2022-01-06 16:40:56 +0100

    hotels project

---
+++ noclegi/src/pages/Search/Search.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { objectToArrayWithId } from '../../helpers/objects';
+import axios from '../../axios';
+import { useState } from 'react';
+import Hotels from '../../components/Hotels/Hotels';
+
+export default function Search(props) {
+  const { term } = useParams();
+  const [hotels, setHotels] = useState([]);
+
+  const search = async () => {
+    try {
+      const res = await axios.get('/hotels.json');
+      const newHotels = objectToArrayWithId(res.data)
+                    .filter(hotel => hotel.name.includes(term));
+      setHotels(newHotels);
+    } catch (ex) {
+      console.log(ex.response);
+    }
+  }
+
+  useEffect(() => {
+    search();
+  }, [term]);
+
+  return (
+    <div>
+      <h2>Wyniki dla frazy "{term}":</h2>
+      <Hotels hotels={hotels} />
+    </div>
+  );
+}
